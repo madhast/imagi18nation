b671ca0d925868a9ed474e6af8e8c8582f7f99be
Mikhail <franke@mrfranke.ru>
2015-07-04 05:13:51 +0300

    add key for lang

--- client/parts/addLangBtn/main.js
+++
@@ -1,6 +0,0 @@
-Template.addLangBtn.events({
-  'click .add-lang-btn': function () {
-    $('.add-lang-modal').modal();
-    return false;
-  }
-});
--- client/parts/addLangModal/main.js
+++ client/parts/addLangModal/main.js
@@ -2,5 +2,9 @@ Template.addLangModal.events({
   'submit form': function () {
     console.log('submit add lang');
     return false;
+  },
+  'click .add-lang-btn': function () {
+    $('.add-lang-modal').modal();
+    return false;
   }
 });
