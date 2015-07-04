87859ad24d88ce32580449ba98f1098341578959
Mikhail <franke@mrfranke.ru>
2015-07-04 07:04:03 +0300

    change list, add data format

--- client/parts/list/main.js
+++ client/parts/list/main.js
@@ -1,5 +1,35 @@
 Template.list.helpers({
   langs: function () {
-    return ['test', 'test1', 'test2','test', 'test1', 'test2','test', 'test1', 'test2']
+    return [{
+      title: 'Без группы',
+      group: [{
+        key: 'REMOVE',
+        translates: [{title: 'Русскикй', key: 'RU', translate: 'Удалить'},
+                     {title: 'Английский', key: 'EN', translate: 'Remove'}]
+      },{
+        key: 'SAVE',
+        translates: [{title: 'Русскикй', key: 'RU', translate: 'Сохранить'},
+                     {title: 'Английский', key: 'EN', translate: 'Save'}]
+      },{
+        key: 'NIGGA',
+        translates: [{title: 'Русскикй', key: 'RU', translate: 'Нигер'},
+                     {title: 'Английский', key: 'EN', translate: 'afro-american'}]
+      }]
+    },{
+      title: 'Формы',
+      group: [{
+        key: 'SEND',
+        translates: [{title: 'Русскикй', key: 'RU', translate: 'Отправить'},
+                     {title: 'Английский', key: 'EN', translate: 'Send'}]
+      },{
+        key: 'CLOSE',
+        translates: [{title: 'Русскикй', key: 'RU', translate: 'Закрыть'},
+                     {title: 'Английский', key: 'EN', translate: 'Close'}]
+      },{
+        key: 'ITS_YOUR_MOM',
+        translates: [{title: 'Русскикй', key: 'RU', translate: 'Перевод'},
+                     {title: 'Английский', key: 'EN', translate: 'Just test'}]
+      }]
+    },]
   }
 });
